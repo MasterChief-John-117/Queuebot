f198beaeeee17f0341c40a752dc88d8acada98ee
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-10-06 08:38:17 -0700

    Added some extensions to Queue

--- QueueBot/Entities/Queue.cs
+++ QueueBot/Entities/Queue.cs
@@ -1,10 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Discord;
 
 namespace QueueBot.Entities
 {
     public class Queue
     {
         public LinkedList<QueueUser> Users;
+
+        public Queue()
+        {
+            Users = new LinkedList<QueueUser>();
+        }
+        public Queue Add(QueueUser user)
+        {
+            Users.AddLast(user);
+            return this;
+        }
+
+        public Queue Add(ulong userId)
+        {
+            Users.AddLast(new QueueUser(QueueBot.Client.GetUser(userId)));
+            return this;
+        }
+
+        public Queue Add(IUser user)
+        {
+            Users.AddLast(new QueueUser(user));
+            return this;
+        }
+
+        public bool Contains(ulong userId)
+        {
+            return this.Users.Select(q => q.User.Id).Contains(userId);
+        }
+
+        public int GetPosition(ulong userId)
+        {
+            return Users.ToList().FindIndex(u => u.User.Id == userId) + 1;
+        }
 
         public override string ToString()
         {
