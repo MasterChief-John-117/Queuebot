637880cbafead38d4b4b042a3d50e014eec8d43b
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-10-06 08:27:53 -0700

    Added ExceptionHandler

---
+++ QueueBot/Entities/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+
+namespace QueueBot.Entities
+{
+    public class ExceptionHandler
+    {
+        public async void HandleCommandError(ICommandContext Context, Exception e)
+        {
+            await QueueBot.Logger.LogErrorMessage(e.ToString());
+            await Context.Channel.TriggerTypingAsync();
+            await Context.Client.GetApplicationInfoAsync().Result.Owner.SendMessageAsync($"User: `{Context.Message.Author}`\nMessage: `{Context.Message.Content}`\n```\n{e}\n```");
+            await Context.Channel.SendMessageAsync(
+                $"Whoops! An error occured. A bug report has been sent to the developer");
+        }
+    }
+}
